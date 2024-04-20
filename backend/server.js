8c9eade110d8645bf190fd76480b2f5dfda7af29
Michaela MeL4 Kurčíková <miskakurcikova82@gmail.com>
2024-04-20 18:37:50 +0200

    basic db query

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,27 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.get('/', (re, res) => {
+const db = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "L0r3m/psUm;",
+    database: 'clothingapp'
+});
+
+app.get('/', (req, res) => {
     return res.json("From backend side");
 });
 
-app.listen(3306, () => {
+app.get('/clothing_item', (req, res) => {
+    const sql = "SELECT * FROM clothing_item";
+    db.query(sql, (err, res) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    });
+});
+
+app.listen(8081, () => {
     console.log("listening");
 });
 
