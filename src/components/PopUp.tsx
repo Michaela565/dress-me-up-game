f684aed61ace01e9987b75dee9a5f2e742fa856e
Michaela MeL4 Kurčíková <miskakurcikova82@gmail.com>
2024-05-17 13:20:22 +0200

    start making the form

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -1,9 +1,12 @@
+import Select from "react-select";
+
 interface Props {
   className: string;
   visibilitySetter: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const PopUp = ({ className, visibilitySetter }: Props) => {
+  const categories = [{ value: "top", label: "Top" }];
   return (
     <div className={className}>
       <div className="popUp-content">
@@ -15,6 +18,18 @@ const PopUp = ({ className, visibilitySetter }: Props) => {
         >
           &times;
         </span>
+        <form action="">
+          <fieldset>
+            <div className="input-holder">
+              <label htmlFor="name">Name:</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="input-holder">
+              <label htmlFor="category">Name:</label>
+              <Select options={categories}></Select>
+            </div>
+          </fieldset>
+        </form>
         <p>Some text in the Modal..</p>
       </div>
     </div>
